24e51044825f0c9528ea4d5e69a8a3bb8fe7ac5d
annapavlyukova <anna.pavlyukova@inbox.ru>
2016-02-21 03:10:20 +0300

    Верстка. Js.
    Не получилось выровнять иконки соц.сетей, после того, как создала
    спрайт.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showHide(element_id) {
+  var obj = document.getElementById(element_id);
+  if (obj.style.display != "block") {
+     obj.style.display = "block";
+   }
+   else obj.style.display = "none";
+ }
+              
